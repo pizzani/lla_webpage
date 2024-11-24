f2bd9789f63ebc8328ffdda2cc2ce784ae92770c
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2024-11-23 21:00:14 -0300

    Desenvolvimento do footer completo.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,10 @@ fetch('components/navbar.html')
         document.getElementById('navbar').innerHTML = data;
     })
     .catch(error => console.error('Erro ao carregar a barra de navegação:', error));
+
+fetch('components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar a barra de navegação:', error));
