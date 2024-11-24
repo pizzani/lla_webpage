f9a59c9bd5c0bca6ab1f4ac443a9764e0c95324e
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2024-11-24 00:51:32 -0300

    Estrutura com funcionalidade implementada completa mobile

--- js/main.js
+++ js/main.js
@@ -5,37 +5,42 @@ fetch('components/contact-top-bar.html')
     })
     .catch(error => console.error('Erro ao carregar a barra de navegação:', error));
 
-    fetch('components/navbar.html')
+fetch('components/navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
-        
+
         const toggleButton = document.querySelector('.navbar-toggle');
         const navbarMenu = document.querySelector('.navbar-menu');
         const closeButton = document.querySelector('.close-btn');
-        const dropdownButtons = document.querySelectorAll('.dropdown .dropbtn');  // All dropdown buttons
+        const dropdownButtons = document.querySelectorAll('.dropdown .dropbtn');
 
         if (toggleButton && navbarMenu && closeButton && dropdownButtons) {
-            // Toggle the navbar menu on button click
             toggleButton.addEventListener('click', () => {
                 navbarMenu.classList.toggle('show');
             });
-
-            // Close the navbar when the close button is clicked
             closeButton.addEventListener('click', () => {
                 navbarMenu.classList.remove('show');
             });
 
-            // Add click event to each dropdown to toggle visibility on mobile
             dropdownButtons.forEach(button => {
                 button.addEventListener('click', (e) => {
                     const dropdownContent = e.target.nextElementSibling;
-                    dropdownContent.classList.toggle('show');  // Toggle visibility for mobile
+                    dropdownContent.classList.toggle('show');
                 });
             });
         } else {
             console.log('Navbar toggle button, menu, or close button not found.');
         }
+
+        window.onscroll = function() {
+            var toggleButton = document.querySelector('.navbar-toggle');
+            if (window.scrollY > 100) {
+                toggleButton.classList.add('floating');
+            } else {
+                toggleButton.classList.remove('floating');
+            }
+        };
     })
     .catch(error => console.error('Erro ao carregar a barra de navegação:', error));
 
@@ -46,30 +51,25 @@ fetch('components/footer.html')
     })
     .catch(error => console.error('Erro ao carregar a barra de navegação:', error));
 
-// Select all dropdown buttons
 const dropdownButtons = document.querySelectorAll('.dropdown .dropbtn');
 
-// Add event listener to toggle dropdown menus on click
 dropdownButtons.forEach(button => {
     button.addEventListener('click', (e) => {
         const dropdownContent = e.target.nextElementSibling;
 
-        // Close other dropdowns before opening the clicked one
         document.querySelectorAll('.dropdown-content').forEach(content => {
             if (content !== dropdownContent) {
                 content.classList.remove('show');
             }
         });
 
-        // Toggle the current dropdown
         dropdownContent.classList.toggle('show');
     });
 });
 
-// Close dropdowns when clicking outside
 document.addEventListener('click', (e) => {
     const isDropdown = e.target.matches('.dropbtn') || e.target.closest('.dropdown');
-    
+
     if (!isDropdown) {
         document.querySelectorAll('.dropdown-content').forEach(content => {
             content.classList.remove('show');
